c8431cee0d84116baa56c8afa07af0a91fdc94ac
Abel Copa <abelw.copa@gmail.com>
2024-01-28 18:30:11 -0400

    Password Encoder User Details Service

--- src/main/java/com/demo/dh/security/config/security/SecurityBeansInjector.java
+++ src/main/java/com/demo/dh/security/config/security/SecurityBeansInjector.java
@@ -1,13 +1,21 @@
 package com.demo.dh.security.config.security;
 
+import com.demo.dh.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class SecurityBeansInjector {
 
     @Bean
@@ -16,11 +24,24 @@ public class SecurityBeansInjector {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
+    public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(null);
         // provider.setPasswordEncoder();
 
         return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            return userRepository.findByUsername(username)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+        };
     }
 }
