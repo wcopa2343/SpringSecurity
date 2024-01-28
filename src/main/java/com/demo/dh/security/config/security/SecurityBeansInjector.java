a583742cf5891154ec4f4d5c1475607d8204a636
Abel Copa <abelw.copa@gmail.com>
2024-01-28 18:20:01 -0400

    Authentication Provider

--- src/main/java/com/demo/dh/security/config/security/SecurityBeansInjector.java
+++ src/main/java/com/demo/dh/security/config/security/SecurityBeansInjector.java
@@ -3,6 +3,8 @@ package com.demo.dh.security.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
@@ -11,5 +13,14 @@ public class SecurityBeansInjector {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(null);
+        // provider.setPasswordEncoder();
+
+        return provider;
     }
 }
