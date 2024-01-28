0c0d26ccbf689bac4d323a592bf55a9919ca7f43
Abel Copa <abelw.copa@gmail.com>
2024-01-28 18:53:47 -0400

    Security Filter Chain

---
+++ src/main/java/com/demo/dh/security/config/security/HttpSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.demo.dh.security.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class HttpSecurityConfig {
+
+    private AuthenticationProvider authenticationProvider;
+
+    public HttpSecurityConfig(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfig -> sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(this.authenticationProvider);
+
+
+        return httpSecurity.build();
+    }
+}
